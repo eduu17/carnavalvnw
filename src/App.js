799e83eaf8eb5866bbdf01edab42476649a5dd01
eduu17 <sranjel74@gmail.com>
2023-02-20 19:26:18 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import bruxa from "./img/bruxa.png";
+import fada from "./img/fada.png";
+import fantasma from "./img/fantasma.png";
+import zumbi from "./img/zumbi.png";
+import pirata from "./img/pirata.png";
+import vampiro from "./img/vampiro.png";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    figure: [
+      {
+        imagem: `${bruxa}`,
+        titulo: "Bruxa",
+      },
+      {
+        imagem: `${fantasma}`,
+        titulo: "Fantasma",
+      },
+      {
+        imagem: `${vampiro}`,
+        titulo: "Vampiro",
+      },
+      {
+        imagem: `${zumbi}`,
+        titulo: "Zumbi",
+      },
+      {
+        imagem: `${fada}`,
+        titulo: "Fada",
+      },
+      {
+        imagem: `${pirata}`,
+        titulo: "Pirata",
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <main>
+        <h1>Carnaval VnW</h1>
+        <section className="map">
+          {this.state.figure.map((item) => (
+            <div className="box">
+              <img src={item.imagem} alt={item.titulo} />
+              <h2>{item.titulo}</h2>
+            </div>
+          ))}
+        </section>
+      </main>
+    );
+  }
 }
 
 export default App;
